edeb96afe4b4ca235668aaa04cff280c3b6112ec
Pavle Pavlovic <pavle.pavlovic@student.howest.be>
2025-01-21 02:40:30 +0100

    Angular Components - template forms - services - observables - structural directives parent/child communication

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,6 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -8,5 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
-    provideHttpClient()]
+    provideHttpClient(),
+    importProvidersFrom([BrowserAnimationsModule])
+  ]
 };
